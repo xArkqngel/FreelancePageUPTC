c823ccf9554a8904683e6a5523f6f665617f893c
xArkqngel <arkqngel343@gmail.com>
2021-11-30 17:33:28 -0500

    Quiz Finished

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Script loaded")
 var formulario = document.getElementById("form-1")
+var color = document.getElementById()
 
 
 formulario.addEventListener("submit", function(event){
@@ -15,4 +16,19 @@ var apellido = document.getElementById("lastname").value;
 document.getElementById('display').innerHTML = 
                     nombre+" - "+apellido;
 console.log(nombre+"-" +apellido);
+}
+
+function swapX() {
+    var x = document.getElementById("clients");
+	x.style.backgroundColor = "#0097A7";
+}
+
+function swapY() {
+    var x = document.getElementById("clients1");
+	x.style.backgroundColor = "#0097A7";
+}
+
+function swapZ() {
+    var x = document.getElementById("clients2");
+	x.style.backgroundColor = "#0097A7";
 }
